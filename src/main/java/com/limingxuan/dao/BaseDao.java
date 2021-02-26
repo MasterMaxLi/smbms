eaf0f203bed567a5119c8236274f7fe7bd0072f8
MasterMaxLi <riwinkids@gmail.com>
2021-02-27 01:20:38 +0800

    修改了操作数据库的公共类

--- src/main/java/com/limingxuan/dao/BaseDao.java
+++ src/main/java/com/limingxuan/dao/BaseDao.java
@@ -47,12 +47,63 @@ public class BaseDao {
 
     //编写查询公共类
     public static ResultSet execute(Connection connection, PreparedStatement preparedStatement, String sql, Object[] params, ResultSet resultSet) throws SQLException {
+        //预编译的sql可直接在后面执行
         preparedStatement = connection.prepareStatement(sql);
         for(int i = 0; i < params.length; i++){
             //setObject,占位符，从1开始,但数组从0开始
             preparedStatement.setObject(i + 1, params[i]);
         }
-        resultSet = preparedStatement.executeQuery(sql);
+        resultSet = preparedStatement.executeQuery();
         return resultSet;
+    }
+
+    //编写增删改公共方法
+    public static int execute(Connection connection, PreparedStatement preparedStatement, String sql, Object[] params) throws SQLException {
+        preparedStatement = connection.prepareStatement(sql);
+        for(int i = 0; i < params.length; i++){
+            //setObject,占位符，从1开始,但数组从0开始
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+        int updateRows = preparedStatement.executeUpdate();
+        return updateRows;
+    }
+
+    //释放资源，关闭连接
+    public static boolean closeResource(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet){
+        boolean flag = true;
+        if(resultSet != null){
+            try {
+                resultSet.close();
+                //GC回收
+                resultSet = null;
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                flag = false;
+            }
+        }
+
+        if(preparedStatement != null){
+            try {
+                preparedStatement.close();
+                //GC回收
+                preparedStatement = null;
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                flag = false;
+            }
+        }
+
+        if(connection != null){
+            try {
+                connection.close();
+                //GC回收
+                connection = null;
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+                flag = false;
+            }
+        }
+
+        return flag;
     }
 }
