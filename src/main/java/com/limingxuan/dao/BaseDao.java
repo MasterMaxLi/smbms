d0d51efb82425e76e53e14d7edd4f1d8560903ad
MasterMaxLi <riwinkids@gmail.com>
2021-02-27 01:08:21 +0800

    添加操作数据库的公共类

---
+++ src/main/java/com/limingxuan/dao/BaseDao.java
@@ -0,0 +1,58 @@
+package com.limingxuan.dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * Creqated by Limingxuan on 2021/2/27
+ *
+ * 操作数据库的公共类
+ */
+public class BaseDao {
+    private static String driver;
+    private static String url;
+    private static String userName;
+    private static String password;
+
+    //静态代码块，启动就会加载
+    static {
+        Properties properties = new Properties();
+        //通过类加载器读取对应的资源
+        InputStream inputStream = BaseDao.class.getClassLoader().getResourceAsStream("db.properties");
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        driver = properties.getProperty("jdbc.driver");
+        url = properties.getProperty("jdbc.url");
+        userName = properties.getProperty("jdbc.username");
+        password = properties.getProperty("jdbc.password");
+    }
+
+    //获取数据库的连接
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    //编写查询公共类
+    public static ResultSet execute(Connection connection, String sql, Object[] params, ResultSet resultSet) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for(int i = 0; i < params.length; i++){
+            //setObject,占位符，从1开始,但数组从0开始
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+        resultSet = preparedStatement.executeQuery(sql);
+        return resultSet;
+    }
+}
